d59dc6c0d8fc815143dd4b876c24a1f808f3e270
AlexanderGese <alexander.gese07@gmail.com>
2026-03-17 09:22:27 +0100

    Gui Start and Working Quit Button Unstyled

---
+++ lastExhibit/scenes/GUI/main_gui.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+# Play
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+# Settings
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+#Quit
+func _on_quit_pressed() -> void:
+	get_tree().quit()
