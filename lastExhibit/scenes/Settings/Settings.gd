80ea26a5495423236278ed7856f2003ef9377ed2
AlexanderGese <alexander.gese07@gmail.com>
2026-03-19 19:56:13 +0100

    added VFX and Master and Music Sliders

--- lastExhibit/scenes/Settings/Settings.gd
+++ lastExhibit/scenes/Settings/Settings.gd
@@ -10,7 +10,13 @@ func _ready() -> void:
 
 func rebind_action(action_name: String, keycode: int):
 	InputMap.action_erase_events(action_name)
-
 	var ev := InputEventKey.new()
 	ev.physical_keycode = keycode
 	InputMap.action_add_event(action_name, ev)
+
+
+
+func _on_button_pressed() -> void:
+	settingsmenu.play("roll up")
+	await get_tree().create_timer(0.1).timeout
+	get_tree().chnge_scene_to_file("res://scenes/GUI/MainGui.tscn")
