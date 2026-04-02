06590525efccafc7aa6f0a3184e761d055cd72a2
Fabian Wolf <fabianwolf.mechenhard@gmx.de>
2026-04-03 01:22:28 +0200

    edited:Aufzug GUI

--- lastExhibit/scenes/Museum/elevator/elevator.gd
+++ lastExhibit/scenes/Museum/elevator/elevator.gd
@@ -1,18 +1,23 @@
 extends Area2D
 
-var player_inside: bool = false
+var player_collided: bool = false
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		player_inside = true
+		player_collided = true
+		$ElevatorPrompt.visible = true
+		#print ("colldide")
+		#print (body.name)
 
 func _on_body_exited(body: Node) -> void:
 	if body.is_in_group("player"):
-		player_inside = false
+		player_collided = false
+		$ElevatorPrompt.visible = false
+		#print ("exit")
 		var ui = get_tree().get_first_node_in_group("player").get_node("ElevatorUI")
 		ui.hide()
 
 func _process(_delta: float) -> void:
-	if player_inside and Input.is_action_just_pressed("interact"):
+	if player_collided and Input.is_action_just_pressed("interact"):
 		var ui = get_tree().get_first_node_in_group("player").get_node("ElevatorUI")
 		ui.show()
--- lastExhibit/scenes/Museum/elevator/elevator_ui.gd
+++ lastExhibit/scenes/Museum/elevator/elevator_ui.gd
@@ -1,5 +1,10 @@
 extends Control
 @onready var player = get_parent()
+@onready var basementbutton = $GridContainer/Basement/AnimatedSprite2D1
+@onready var floor0button = $GridContainer/Floor0/AnimatedSprite2D2
+@onready var floor1button = $GridContainer/Floor1/AnimatedSprite2D3
+@onready var floor2button = $GridContainer/Floor2/AnimatedSprite2D4
+
 
 func _ready() -> void:
 	hide()
@@ -13,17 +18,25 @@ func _on_floor_button_pressed(floor: int) -> void:
 	hide()
 
 func _on_floor_2_pressed() -> void:
+	floor2button.play("floor2")
+	await get_tree().create_timer(0.75).timeout
 	player.global_position = Vector2(-98.0 , -2227.0)
 	hide()
 
 func _on_floor_1_pressed() -> void:
+	floor1button.play("floor1")
+	await get_tree().create_timer(0.75).timeout
 	player.global_position = Vector2(-115.0 , -1591.0)
 	hide()
-	
+
 func _on_floor_0_pressed() -> void:
+	floor0button.play("floor0")
+	await get_tree().create_timer(0.75).timeout
 	player.global_position = Vector2(-9.0,-923.0)
 	hide()
 
 func _on_basement_pressed() -> void:
+	basementbutton.play("basement")
+	await get_tree().create_timer(0.75).timeout
 	player.global_position = Vector2(47.0, -246.0)
 	hide()
