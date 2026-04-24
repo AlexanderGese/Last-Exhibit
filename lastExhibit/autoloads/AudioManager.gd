15a5af6f505c2b25428b1ffe2719221b59bb721b
AlexanderGese <alexander.gese07@gmail.com>
2026-04-24 10:11:58 +0200

    changed audiomanager

--- lastExhibit/autoloads/AudioManager.gd
+++ lastExhibit/autoloads/AudioManager.gd
@@ -7,19 +7,19 @@ extends Node
 var current_track: String = ""
 
 const TRACKS = {
-	"museum_day":    "res://assets/audio/music/museum_day.ogg",
-	"museum_night":  "res://assets/audio/music/museum_night.ogg",
-	"soviet":        "res://assets/audio/music/soviet.ogg",
-	"soviet_boss":   "res://assets/audio/music/soviet_boss.ogg",
-	"ww2":           "res://assets/audio/music/ww2.ogg",
-	"ww2_boss":      "res://assets/audio/music/ww2_boss.ogg",
-	"medieval":      "res://assets/audio/music/medieval.ogg",
-	"medieval_boss": "res://assets/audio/music/medieval_boss.ogg",
-	"samurai":       "res://assets/audio/music/samurai.ogg",
-	"samurai_boss":  "res://assets/audio/music/samurai_boss.ogg",
-	"inka":          "res://assets/audio/music/inka.ogg",
-	"inka_boss":     "res://assets/audio/music/inka_boss.ogg",
-	"blackmarket":   "res://assets/audio/music/blackmarket.ogg",
+	"museum_day":    "res://assets/music/MuseumDay.ogg",
+	"museum_night":  "res://assets/music/MuseumNight.ogg",
+	"soviet":        "res://assets/music/SowjetLevel.ogg",
+	"soviet_boss":   "res://assets/music/SowjetBoss.ogg",
+	"ww2":           "res://assets/music/WW2Level.ogg",
+	"ww2_boss":      "res://assets/music/WW2Boss.ogg",
+	"medieval":      "res://assets/music/MedivalLevel.ogg",
+	"medieval_boss": "res://assets/music/MedivalBoss.ogg",
+	"samurai":       "res://assets/music/JapanLevel.ogg",
+	"samurai_boss":  "res://assets/music/JapanBoss.ogg",
+	"inka":          "res://assets/music/InkaLevel.ogg",
+	"inka_boss":     "res://assets/music/InkaBoss.ogg",
+	"blackmarket":   "res://assets/music/BlackMarketMusic.ogg",
 }
 
 func play(track_name: String, fade: bool = true) -> void:
