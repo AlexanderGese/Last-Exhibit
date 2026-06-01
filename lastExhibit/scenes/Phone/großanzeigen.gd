22cfe38eb5748db7606a681a1d69c24ae0daf134
AlexanderGese <alexander.gese07@gmail.com>
2026-06-01 19:32:08 +0200

    groß

---
+++ lastExhibit/scenes/Phone/großanzeigen.gd
@@ -0,0 +1,39 @@
+extends TextureRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func button_1() -> void:
+	pass # Replace with function body.
+
+
+func button_2() -> void:
+	pass # Replace with function body.
+
+
+func button_3() -> void:
+	pass # Replace with function body.
+
+
+func button_4() -> void:
+	pass # Replace with function body.
+
+
+func button_5() -> void:
+	pass # Replace with function body.
+
+
+func button_6() -> void:
+	pass # Replace with function body.
+
+
+func button_7() -> void:
+	pass # Replace with function body.
