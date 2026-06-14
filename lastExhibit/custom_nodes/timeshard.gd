4d85d44524ae70ef4aacd89a4335e23d93fe7524
AlexanderGese <alexander.gese07@gmail.com>
2026-06-15 00:58:50 +0200

    return pad fix

---
+++ lastExhibit/custom_nodes/timeshard.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var in_area:= false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if in_area and Input.is_action_just_pressed("interact"):
+		in_area = false
+		$CollisionShape2D.disabled = true
+		SaveManager.player.time_shards += 1
+		$Sprite2D.visible = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		in_area = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		in_area = false
