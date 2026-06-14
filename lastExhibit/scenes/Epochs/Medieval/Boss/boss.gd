793010f6e378ba6162720253f9bd8c280455350e
Paul Korn <kornpaul2310@gmail.com>
2026-06-15 00:23:31 +0200

    Boss_Medival_2

--- lastExhibit/scenes/Epochs/Medieval/Boss/boss.gd
+++ lastExhibit/scenes/Epochs/Medieval/Boss/boss.gd
@@ -2,12 +2,18 @@
 extends CharacterBody2D
 
 @export var walk_speed  : float = 180.0
-@export var run_speed   : float = 350.0
+@export var run_speed   : float = 280.0
 @export var gravity     : float = 980.0
 @export var min_x       : float = 4160.0
 @export var max_x       : float = 4830.0
-@export var kampf_dist  : float = 64.0
-@export var run_distanz : float = 128.0
+@export var kampf_dist  : float = 50.0
+@export var run_distanz : float = 100.0
+
+@export var hit_cooldown  : float = 3
+@export var hit_distanz   : float = 70.0
+
+var hit_timer    : float = 0.0
+var is_attacking : bool  = false
 
 var player : CharacterBody2D
 var facing_right : bool = true
@@ -27,14 +33,35 @@ func _physics_process(delta: float) -> void:
 
 	if player == null:
 		return
-
-	_move_towards_player()
+	hit_timer  -= delta
+	
+	if hit_timer<0:
+		_hit_start()
+	
+	if is_attacking:
+		velocity.x = 0.0
+	else:
+		_move_towards_player()
 	_update_facing()
 	move_and_slide()
 
+func _hit_start() -> void:
+	is_attacking = true
+	hit_timer    = hit_cooldown
+	$Hitbox.set_deferred("disabled", true)
+	velocity.x   = 0.0
+	if randf() < 0.5:
+		$AnimatedSprite2D.play("attack1")
+	else:
+		$AnimatedSprite2D.play("attack2")
+	await $AnimatedSprite2D.animation_finished
+	$Hitbox.set_deferred("disabled", false)
+	is_attacking = false
+
+
 func _move_towards_player() -> void:
-	var dist = global_position.distance_to(player.global_position)
-	var dir  = sign(player.global_position.x - global_position.x)
+	var dist_x = abs(global_position.x - player.global_position.x)
+	var dir    = sign(player.global_position.x - global_position.x)
 
 	if global_position.x <= min_x and dir < 0:
 		velocity.x = 0.0
@@ -43,12 +70,12 @@ func _move_towards_player() -> void:
 		velocity.x = 0.0
 		return
 
-	if dist <= kampf_dist:
+	if dist_x <= kampf_dist:
 		velocity.x = move_toward(velocity.x, 0.0, 20.0)
 		$AnimatedSprite2D.play("idle")
 		return
 
-	if dist > run_distanz:
+	if dist_x > run_distanz:
 		velocity.x = dir * run_speed
 		$AnimatedSprite2D.play("run")
 	else:
@@ -63,3 +90,4 @@ func _update_facing() -> void:
 	else:
 		facing_right = player.global_position.x > global_position.x
 	$AnimatedSprite2D.flip_h = not facing_right
+		
