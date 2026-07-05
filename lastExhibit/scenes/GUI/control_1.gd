14b79a0be0f7f07036dc69f3833e6d68c67a8157
Fabian Wolf <fabianwolf.mechenhard@gmx.de>
2026-07-06 00:26:16 +0200

    edited intro 2

--- lastExhibit/scenes/GUI/control_1.gd
+++ lastExhibit/scenes/GUI/control_1.gd
@@ -4,5 +4,5 @@ extends Control
 const NEXT_SCENE_PATH = "res://scenes/GUI/Intro2.tscn"
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "intro_start":
+	if anim_name == "intro1":
 		get_tree().change_scene_to_file(NEXT_SCENE_PATH)
