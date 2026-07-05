afe1fbd28d46714b2884fe53e778936e4c11e0f3
Fabian Wolf <fabianwolf.mechenhard@gmx.de>
2026-07-05 17:20:12 +0200

    added Intro1

---
+++ lastExhibit/scenes/GUI/control.gd
@@ -0,0 +1,8 @@
+extends Control
+
+#HIER PFAD AUSTAUSCHEN
+const NEXT_SCENE_PATH = "res://scenes/GUI/MainGui.tscn"
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "intro_start":
+		get_tree().change_scene_to_file(NEXT_SCENE_PATH)
